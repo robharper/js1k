785c034448c7c351a5fa2db48a97d44eb8d1aabc
Rob Harper <robert.harper@gmail.com>
2014-02-06 16:33:05 -0500

    Put demos on gh pages

---
+++ js/shadebobs_tiny.js
@@ -0,0 +1,1 @@
+with(c=Math.cos,s=Math.sin,l=a=0,z=.06,document.body.children[0].getContext("2d"))globalCompositeOperation="lighter",fillStyle="#05050A",scale(320,240),setInterval(function(){for(clearRect(0,0,2,2);a>l;l+=.01)fillRect(x=-c(l)/2-c(l/4)/2+1,y=s(.9*l)*(c(.8*l+1)+2)/4+1,z,z),fillRect(2-x,2-y,z,z);a+=.5,l-=22},33);
