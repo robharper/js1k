e8e5dcfebe1740d2b262906f12d03f95da0a75dc
robharper <robert.harper@gmail.com>
2024-04-01 17:04:35 -0400

    Bitscape 2d raycasting;

--- src/js/bitscape.js
+++ src/js/bitscape.js
@@ -1,44 +1,85 @@
 var draw = t => {
   c.width |= 0;
-  W = 240
-  H = 135
+  W = 120
+  H = 70
   P = 0;
 
-  x_pattern = 13256 * (1.1 + S(t/3|0)) % (1<<16);
-  y_pattern = 972 * (1.7 + S(t/2|0)) % (1<<16);
+  // x_pattern = [
+  //   0,
+  //   0b000111110011111100111110000000,
+  //   0b000000100110000001010101000000,
+  //   0b000000100011110001001001000000,
+  //   0b000101100000011001100011000000,
+  //   0b000111001111110000111110000000,
+  //   0
+  // ];
+  x_pattern = [
+    0b00001111100000100000011100000000,
+    0b00001110000001110000011100000000,
+    0b00001100000011100000011100000000,
+    0b00001000000111000000011100000000,
+  ]
+  //y_pattern = 0b00000011111110001111000100000000;
 
-  for (i=4e4; i--;) {
+  posX = 16 + 5*C(t);
+  posY = 5 + 2*S(t*1.7);
+  posZ = -10 + 3*t;
+
+  for (i=8400; i--;) {
     xp = i % W;
     yp = i / W | 0;
 
-    // Start at the front, work back, stop when we hit a solid
-    for (depth=8; --depth;) {
-      d = (depth / 16) + .5;
-      xd = ((xp - W/2) / d + W/2 + t*40) % W;
-      yd = ((yp - H/2) / d + H/2 + t*20) % H;
-      // Screen to world
-      xw = 1 << xd / 15;
-      yw = 1 << yd / 15;
-      if (xw & x_pattern && yw & y_pattern) {
-        // Hit solid
-        x.fillRect(
-          xp*8+ 1/(yp-(30*(C(t) + S(1.7*t) - 3*t)%H) - H)*H,
-          yp*8,
-          depth*2,
-          depth,
-          );
-        P += depth;
+    // Calculate ray
+    // Camera position: 0,0,0
+    // Camera facing 0,0,1 (positive z into screen)
+    rayX = 2*xp/W - 1;
+    rayY = 2*yp/H - 1;
+    rayZ = 1;
+
+    deltaDistX = Math.abs(1 / rayX);
+    deltaDistY = Math.abs(1 / rayY);
+    deltaDistZ = 1;
+
+    mapX = posX|0;
+    mapY = posY|0;
+    mapZ = posZ|0;
+
+    if (rayX < 0) {
+      stepX = -1;
+      sideDistX = (posX - mapX) * deltaDistX;
+    } else {
+      stepX = 1;
+      sideDistX = (mapX+1 - posX) * deltaDistX;
+    }
+    if (rayY < 0) {
+      stepY = -1;
+      sideDistY = (posY - mapY) * deltaDistY;
+    } else {
+      stepY = 1;
+      sideDistY = (mapY + 1 - posY) * deltaDistY;
+    }
+    sideDistZ = (mapZ + 1 - posZ);
+
+    while (sideDistZ < 20 && mapX > 0 && mapX < 32 && mapY > 0 && mapY < 10) {
+      if (sideDistX < sideDistZ && sideDistX < sideDistY) {
+        dist = sideDistX;
+        sideDistX += deltaDistX;
+        mapX += stepX;
+      } else if (sideDistY < sideDistZ) {
+        dist = sideDistY;
+        sideDistY += deltaDistY;
+        mapY += stepY;
+      } else {
+        dist = sideDistZ;
+        sideDistZ += 1;
+        mapZ += 1;
+      }
+      if (mapZ-posZ < 2) continue;
+      //Check if ray has hit a wall
+      if ((x_pattern[mapY/2|0] >> 32-mapX) & 1 && mapZ % 7 < 2 || mapY === 10) {
+        x.fillRect(xp*16, yp*16, 18-dist, 18-dist);
         break;
       }
     }
-    // Add noise where no solid drawn
-    depth || C(yp+t*100) > 0.9 &&
-      x.fillRect(xp*8, yp*8, 8*S(xp*yp), 1, P+=1);
   }
 }
-
-// K = W/2 + t*40;
-// C = W/2;
-// D = "xd" value calulated by finding depth zero and offsetting by direction of focal point;
-
-// d = (xp - C) / (D-K);
