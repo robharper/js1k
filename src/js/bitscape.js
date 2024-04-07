5df6c032c0fb0a5c688a20786a2d04eca180d8e1
robharper <robert.harper@gmail.com>
2024-04-07 13:51:37 -0400

    Bitscape pre-crush

--- src/js/bitscape.js
+++ src/js/bitscape.js
@@ -4,41 +4,20 @@ var draw = t => {
   H = 70
   P = 0;
 
-  // x_pattern = [
-  //   0,
-  //   0b000111110011111100111110000000,
-  //   0b000000100110000001010101000000,
-  //   0b000000100011110001001001000000,
-  //   0b000101100000011001100011000000,
-  //   0b000111001111110000111110000000,
-  //   0
-  // ];
-  x_pattern = [
-    0b00001111100000100000011100000000,
-    0b00001110000001110000011100000000,
-    0b00001100000011100000011100000000,
-    0b00001000000111000000011100000000,
-  ]
-  //y_pattern = 0b00000011111110001111000100000000;
-
-  posX = 16 + 5*C(t);
-  posY = 5 + 2*S(t*1.7);
-  posZ = -10 + 3*t;
+  posX = 13 + 5*C(t);
+  posY = 3 + 2*S(t*1.7);
+  posZ = 3 + 3*t;
 
   for (i=8400; i--;) {
     xp = i % W;
     yp = i / W | 0;
 
-    // Calculate ray
-    // Camera position: 0,0,0
-    // Camera facing 0,0,1 (positive z into screen)
     rayX = 2*xp/W - 1;
-    rayY = 2*yp/H - 1;
+    rayY = 2*yp/H - 1 + 0.75;
     rayZ = 1;
 
     deltaDistX = Math.abs(1 / rayX);
     deltaDistY = Math.abs(1 / rayY);
-    deltaDistZ = 1;
 
     mapX = posX|0;
     mapY = posY|0;
@@ -60,23 +39,25 @@ var draw = t => {
     }
     sideDistZ = (mapZ + 1 - posZ);
 
-    while (sideDistZ < 20 && mapX > 0 && mapX < 32 && mapY > 0 && mapY < 10) {
+    while (sideDistZ < 20 && mapX > -5 && mapX < 32 && mapY > 0 && mapY < 13) {
       if (sideDistX < sideDistZ && sideDistX < sideDistY) {
         dist = sideDistX;
         sideDistX += deltaDistX;
         mapX += stepX;
+        side = 0;
       } else if (sideDistY < sideDistZ) {
         dist = sideDistY;
         sideDistY += deltaDistY;
         mapY += stepY;
+        side = 1;
       } else {
         dist = sideDistZ;
         sideDistZ += 1;
         mapZ += 1;
+        side = 2;
       }
-      if (mapZ-posZ < 2) continue;
-      //Check if ray has hit a wall
-      if ((x_pattern[mapY/2|0] >> 32-mapX) & 1 && mapZ % 7 < 2 || mapY === 10) {
+      if (C(mapX*mapZ) > 0.5 && mapY > 10 - 8*S(mapX*mapZ) || mapY === 12) {
+        x.fillStyle = side == 2 ? '#000' : mapY === 12 ? '#1a1' : '#666';
         x.fillRect(xp*16, yp*16, 18-dist, 18-dist);
         break;
       }
