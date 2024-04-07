4c58554011e40572c90b60ed29a4c6cd78f183bd
robharper <robert.harper@gmail.com>
2024-04-07 15:11:57 -0400

    Minor golfing, bitscape to 351 bytes

--- src/js/bitscape.js
+++ src/js/bitscape.js
@@ -1,3 +1,5 @@
+var W, H, P, posX, posY, posZ, xp, yp, rayX, rayY, mapX, mapY, mapZ, deltaDistX, deltaDistY, sideDistX, sideDistY, sideDistZ, side, RND, SQUARE;
+
 var draw = t => {
   c.width |= 0;
   W = 120
@@ -8,57 +10,39 @@ var draw = t => {
   posY = 3 + 2*S(t*1.7);
   posZ = 3 + 3*t;
 
-  for (i=8400; i--;) {
+  for (i=9e3; i--;) {
     xp = i % W;
     yp = i / W | 0;
 
-    rayX = 2*xp/W - 1;
-    rayY = 2*yp/H - 1 + 0.75;
-    rayZ = 1;
-
-    deltaDistX = Math.abs(1 / rayX);
-    deltaDistY = Math.abs(1 / rayY);
+    rayX = 2*xp/W;
+    rayY = 2*yp/H;
 
     mapX = posX|0;
     mapY = posY|0;
     mapZ = posZ|0;
 
-    if (rayX < 0) {
-      stepX = -1;
-      sideDistX = (posX - mapX) * deltaDistX;
-    } else {
-      stepX = 1;
-      sideDistX = (mapX+1 - posX) * deltaDistX;
-    }
-    if (rayY < 0) {
-      stepY = -1;
-      sideDistY = (posY - mapY) * deltaDistY;
-    } else {
-      stepY = 1;
-      sideDistY = (mapY + 1 - posY) * deltaDistY;
-    }
-    sideDistZ = (mapZ + 1 - posZ);
+    deltaDistX = 1 / rayX;
+    deltaDistY = 1 / rayY;
+    sideDistX = (mapX-posX+1) * deltaDistX;
+    sideDistY = (mapY-posY+1) * deltaDistY;
+    sideDistZ = (mapZ-posZ+1);
 
-    while (sideDistZ < 20 && mapX > -5 && mapX < 32 && mapY > 0 && mapY < 13) {
+    while (sideDistZ < 18) {
+      side = 0;
       if (sideDistX < sideDistZ && sideDistX < sideDistY) {
-        dist = sideDistX;
         sideDistX += deltaDistX;
-        mapX += stepX;
-        side = 0;
+        mapX += 1;
       } else if (sideDistY < sideDistZ) {
-        dist = sideDistY;
         sideDistY += deltaDistY;
-        mapY += stepY;
-        side = 1;
+        mapY += 1;
       } else {
-        dist = sideDistZ;
         sideDistZ += 1;
         mapZ += 1;
-        side = 2;
+        side = 1;
       }
-      if (C(mapX*mapZ) > 0.5 && mapY > 10 - 8*S(mapX*mapZ) || mapY === 12) {
-        x.fillStyle = side == 2 ? '#000' : mapY === 12 ? '#1a1' : '#666';
-        x.fillRect(xp*16, yp*16, 18-dist, 18-dist);
+      if (C(RND = mapX*mapZ) > 0.5 && mapY > 10 - 8*S(RND) || mapY === 12) {
+        x.fillStyle = mapY === 12 ? '#1a1' : side ? '#000' : '#666';
+        x.fillRect(xp*16, yp*16, SQUARE=18-sideDistZ, SQUARE);
         break;
       }
     }
